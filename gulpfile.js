d2756ce970cc822e880c21b08ab67030e8cab5cd
Dan Braun <danbraun@users.noreply.github.com>
2022-02-06 08:40:28 -0600

    added FA edited animations

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,27 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass')(require('sass'));
 
-gulp.task('sass', function(cb) {
+gulp.task('sass', function (cb) {
   gulp
     .src('*.scss')
     .pipe(sass())
     .pipe(
-      gulp.dest(function(f) {
+      gulp.dest(function (f) {
         return f.base;
       })
     );
   cb();
 });
 
+gulp.task('copy-fa', function (cb) {
+  gulp.src('./node_modules/@fortawesome/fontawesome-free/webfonts/**/*')
+    .pipe(gulp.dest('./webfonts/'));
+  cb();
+});
+
 gulp.task(
   'default',
-  gulp.series('sass', function(cb) {
+  gulp.series('sass', function (cb) {
     gulp.watch('*.scss', gulp.series('sass'));
     cb();
   })
